56ea9c5777859d333c7bfe89dda8aa497702ea47
Olivier HEMAR <olivier@deepalgo.com>
2020-04-09 07:45:51 +0000

    Update CurrencyUtils.java //@deepalgo

--- CurrencyUtils.java
+++ CurrencyUtils.java
@@ -7,16 +7,16 @@ public class CurrencyUtils {
 public class Currency {
     private static HashMap<String, Currency> listOfCurrencies;
     private static HashMap<String, Double> listOfVAT;
-    
+    //@deepalgo
     public static Currency currencyByCountryCode(String country){
         return listOfCurrencies.getCurrency(country);
     }
-
+//@deepalgo
     public static Double vatByCountryCode(Sring country){
         Double countryVat = listOfVAT.getVAT(country);
         return countryVat;
     }
-
+//@deepalgo
     public static Double currencyRate(String fromCountry, String toCountry, Date today){
         MarketProvider mkt = reutersConnector();
         Double currencyRate = mkt.currencyRate(fromCountry, toCountry, today);
